667a3a8c3ad2067337896e4dad91a2452d8d9e1e
ajay-gov <ajay_gov@yahoo.com>
2019-02-20 10:46:21 -0800

    Fix run scripts

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ cd ui
 npm install
 npm run compile && npm run bundle
 mkdir -p ../server/ui
-cp -rf index.html dist/ ../server/ui/
+cp -rf index.html dist ../server/ui/
 cd ../server
 ./gradlew run
 

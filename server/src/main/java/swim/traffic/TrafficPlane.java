5108c91585ba70cb34a4fffe97ec3cbb639a3f34
ajay-gov <ajay.gov@protonmail.com>
2023-08-29 18:38:12 -0700

    Replace AgentRoute annotations with node paths in the server.recon file Replace deprecated APIs in gradle file

--- server/src/main/java/swim/traffic/TrafficPlane.java
+++ server/src/main/java/swim/traffic/TrafficPlane.java
@@ -14,25 +14,13 @@
 
 package swim.traffic;
 
-import swim.api.SwimAgent;
-import swim.api.SwimRoute;
-import swim.api.agent.AgentRoute;
 import swim.api.plane.AbstractPlane;
 import swim.api.space.Space;
 import swim.kernel.Kernel;
 import swim.server.ServerLoader;
 import swim.structure.Value;
-import swim.traffic.agent.CityAgent;
-import swim.traffic.agent.IntersectionAgent;
 
 public class TrafficPlane extends AbstractPlane {
-  @SwimAgent("city")
-  @SwimRoute("/city/:id")
-  AgentRoute<CityAgent> cityAgent;
-
-  @SwimAgent("intersection")
-  @SwimRoute("/intersection/:country/:state/:city/:id")
-  AgentRoute<IntersectionAgent> intersectionAgent;
 
   public static void main(String[] args) {
     final Kernel kernel = ServerLoader.loadServer();

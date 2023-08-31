59432076f903e755f5e1b085f85b651178410e78
Dobromir Marinov <mr.d.marinov@gmail.com>
2023-08-31 17:36:47 +0100

    Updated run script.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ npm install
 npm run compile && npm run bundle
 mkdir -p ../server/ui
 cp -rf index.html dist ../server/ui/
-cp -rf login.html dist ../server/ui/
+cp -rf app.html dist ../server/ui/
 cd ../server
 ./gradlew clean run
 
